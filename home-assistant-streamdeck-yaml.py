aca9a5e6f38e02e23d87737434fdc05430b270b5
Bas Nijholt <bas@nijho.lt>
2023-03-12 01:10:41 -0800

    use pydantic for config

--- home-assistant-streamdeck-yaml.py
+++ home-assistant-streamdeck-yaml.py
@@ -1,17 +1,22 @@
+from __future__ import annotations
+
 import asyncio
 import json
 from contextlib import asynccontextmanager
 from pathlib import Path
-from typing import Any
+from typing import TYPE_CHECKING, Any
 
 import rich
 import websockets
 import yaml
 from jinja2 import Template
 from PIL import Image, ImageDraw, ImageFont
+from pydantic import BaseModel, Field
 from StreamDeck.DeviceManager import DeviceManager
 from StreamDeck.ImageHelpers import PILHelper
-from StreamDeck.Devices import StreamDeck
+
+if TYPE_CHECKING:
+    from StreamDeck.Devices import StreamDeck
 
 ASSETS_PATH = Path(__file__).parent / "assets"
 
@@ -19,7 +24,21 @@ ASSETS_PATH = Path(__file__).parent / "assets"
 _ID_COUNTER = 0
 
 
-def next_id():
+class Button(BaseModel, extra="forbid"):
+    entity_id: str | None = None
+    service: str | None = None
+    icon: str | None = None
+    text: str = ""
+    text_color: str | None = None
+    service_data: dict[str, Any] = Field(default_factory=dict)
+    data: dict[str, Any] = Field(default_factory=dict)
+
+
+class Config(BaseModel):
+    buttons: list[Button] = Field(default_factory=list)
+
+
+def next_id() -> int:
     global _ID_COUNTER
     _ID_COUNTER += 1
     return _ID_COUNTER
@@ -72,10 +91,10 @@ async def handle_state_changes(
         _update_state(state, data, buttons, deck)
 
 
-def _key(entity_id: str, buttons) -> int | None:
+def _key(entity_id: str, buttons: list[Button]) -> int | None:
     """Get the key index for an entity_id."""
     for i, button in enumerate(buttons):
-        if button.get("entity") == entity_id:
+        if button.entity_id == entity_id:
             return i
     return None
 
@@ -124,8 +143,7 @@ async def get_states(websocket):
             return state_dict
 
 
-async def unsubscribe(websocket: websockets.WebSocketClientProtocol, id: str):
-    ...
+async def unsubscribe(websocket: websockets.WebSocketClientProtocol, id: int):
     # Subscribe to the state change events for a specific entity
     subscribe_payload = {
         "id": next_id(),
@@ -135,8 +153,11 @@ async def unsubscribe(websocket: websockets.WebSocketClientProtocol, id: str):
     await websocket.send(json.dumps(subscribe_payload))
 
 
-async def call_service(websocket, domain, service, data):
-    # Subscribe to the state change events for a specific entity
+async def call_service(
+    websocket: websockets.WebSocketClientProtocol, service: str, data: dict[str, Any],
+):
+    """Call a service."""
+    domain, service = service.split(".")
     subscribe_payload = {
         "id": next_id(),
         "type": "call_service",
@@ -148,7 +169,7 @@ async def call_service(websocket, domain, service, data):
 
 
 def render_key_image(
-    deck,
+    deck: StreamDeck,
     *,
     text_color: str = "white",
     icon_filename: str | None = None,
@@ -182,31 +203,37 @@ def render_key_image(
     return PILHelper.to_native_format(deck, image)
 
 
-def update_key_image(deck, key, button, state, key_pressed: bool = False):
-    if button.get("entity") in state:
-        state = state[button["entity"]]
-        text = _render_jinja(button["text"], data={"state": state})
-        if "text_color" in button:
-            text_color = _render_jinja(button["text_color"], data={"state": state})
+def update_key_image(
+    deck: StreamDeck,
+    key: int,
+    button: Button,
+    state: dict[str, Any],
+    key_pressed: bool = False,
+):
+    if button.entity_id in state:
+        state = state[button.entity_id]
+        text = _render_jinja(button.text, data={"state": state})
+        if button.text_color is not None:
+            text_color = _render_jinja(button.text_color, data={"state": state})
         else:
             if state["state"] == "on":
                 text_color = "orangered"
             else:
                 text_color = "white"
     else:
-        text = button.get("text", "")
-        text_color = button.get("text_color", "white")
+        text = button.text
+        text_color = button.text_color or "white"
     image = render_key_image(
         deck,
         label_text=text,
         text_color=text_color if not key_pressed else "green",
-        icon_filename=button.get("icon"),
+        icon_filename=button.icon,
     )
     with deck:
         deck.set_key_image(key, image)
 
 
-def get_deck():
+def get_deck() -> StreamDeck:
     """Get the first Stream Deck device found on the system."""
     streamdecks = DeviceManager().enumerate()
     found = False
@@ -223,32 +250,33 @@ def get_deck():
     return deck
 
 
-def read_config(fname: str):
+def read_config(fname: str) -> Config:
     with open(fname) as f:
-        return yaml.safe_load(f)
+        return Config(buttons=yaml.safe_load(f))
 
 
-async def main(host, token, config):
+async def main(host: str, token: str, config: Config):
     deck = get_deck()
-    buttons = config  # TODO: make it work with multiple decks
+    buttons = config.buttons  # TODO: make it work with multiple decks
     async with setup_ws(host, token) as websocket:
         state = await get_states(websocket)
         for key in range(deck.key_count()):
-            update_key_image(deck, key, buttons[key], state, False)
-        deck.set_key_callback_async(on_press_callback(websocket, state, buttons))
+            update_key_image(deck, key, buttons[key], state, key_pressed=False)
+        deck.set_key_callback_async(_on_press_callback(websocket, state, buttons))
         deck.set_brightness(100)
         await subscribe_state_changes(websocket)
         await handle_state_changes(websocket, state, deck, buttons)
 
 
-def on_press_callback(websocket, state, buttons):
+def _on_press_callback(websocket, state, buttons):
     async def key_change_callback(deck, key, key_pressed):
         print(f"Key {key} {'pressed' if key_pressed else 'released'}")
         button = buttons[key]
         update_key_image(deck, key, button, state, key_pressed)
         if key_pressed:
-            data = {"entity_id": button["entity"]} if "entity" in button else {}
-            await call_service(websocket, *button["service"].split("."), data)
+            rich.print("Calling service", button.service)
+            data = {"entity_id": button.entity_id} if button.entity_id else {}
+            await call_service(websocket, button.service, data)
 
     return key_change_callback
 

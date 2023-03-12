08b082dffe5faae6597ef6f7408cca771c40bf8b
Bas Nijholt <bas@nijho.lt>
2023-03-12 01:24:01 -0800

    fix all ruff problems

--- home-assistant-streamdeck-yaml.py
+++ home-assistant-streamdeck-yaml.py
@@ -1,10 +1,11 @@
+"""Home Assistant Stream Deck integration."""
 from __future__ import annotations
 
 import asyncio
 import json
 from contextlib import asynccontextmanager
 from pathlib import Path
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING, Any, Callable
 
 import rich
 import websockets
@@ -25,6 +26,8 @@ _ID_COUNTER = 0
 
 
 class Button(BaseModel, extra="forbid"):
+    """Button configuration."""
+
     entity_id: str | None = None
     service: str | None = None
     icon: str | None = None
@@ -35,10 +38,12 @@ class Button(BaseModel, extra="forbid"):
 
 
 class Config(BaseModel):
+    """Configuration file."""
+
     buttons: list[Button] = Field(default_factory=list)
 
 
-def next_id() -> int:
+def _next_id() -> int:
     global _ID_COUNTER
     _ID_COUNTER += 1
     return _ID_COUNTER
@@ -73,7 +78,7 @@ async def subscribe_state_changes(
     subscribe_payload = {
         "type": "subscribe_events",
         "event_type": "state_changed",
-        "id": next_id(),
+        "id": _next_id(),
     }
     await websocket.send(json.dumps(subscribe_payload))
 
@@ -104,7 +109,7 @@ def _update_state(
     data: dict[str, Any],
     buttons: list[dict[str, Any]],
     deck: StreamDeck,
-):
+) -> None:
     """Update the state dictionary and update the keys."""
     if data["type"] == "event":
         event_data = data["event"]
@@ -116,7 +121,13 @@ def _update_state(
             if key is None:
                 return
             rich.print(f"Updating key {key} for {eid}")
-            update_key_image(deck, key, buttons[key], state, False)
+            update_key_image(
+                deck,
+                key=key,
+                button=buttons[key],
+                state=state,
+                key_pressed=False,
+            )
 
 
 def _render_jinja(text: str, data: dict[str, Any]) -> str:
@@ -125,9 +136,9 @@ def _render_jinja(text: str, data: dict[str, Any]) -> str:
     return template.render(**data)
 
 
-async def get_states(websocket):
-    # Subscribe to the state change events for a specific entity
-    _id = next_id()
+async def get_states(websocket: websockets.WebSocketClientProtocol) -> dict[str, Any]:
+    """Get the current state of all entities."""
+    _id = _next_id()
     subscribe_payload = {
         "type": "get_states",
         "id": _id,
@@ -143,23 +154,25 @@ async def get_states(websocket):
             return state_dict
 
 
-async def unsubscribe(websocket: websockets.WebSocketClientProtocol, id: int):
-    # Subscribe to the state change events for a specific entity
+async def unsubscribe(websocket: websockets.WebSocketClientProtocol, id_: int) -> None:
+    """Unsubscribe from an event."""
     subscribe_payload = {
-        "id": next_id(),
+        "id": _next_id(),
         "type": "unsubscribe_events",
-        "subscription": id,
+        "subscription": id_,
     }
     await websocket.send(json.dumps(subscribe_payload))
 
 
 async def call_service(
-    websocket: websockets.WebSocketClientProtocol, service: str, data: dict[str, Any],
-):
+    websocket: websockets.WebSocketClientProtocol,
+    service: str,
+    data: dict[str, Any],
+) -> None:
     """Call a service."""
     domain, service = service.split(".")
     subscribe_payload = {
-        "id": next_id(),
+        "id": _next_id(),
         "type": "call_service",
         "domain": domain,
         "service": service,
@@ -175,19 +188,13 @@ def render_key_image(
     icon_filename: str | None = None,
     font_filename: str = "Roboto-Regular.ttf",
     label_text: str = "",
-):
-    # Resize the source image asset to best-fit the dimensions of a single key,
-    # leaving a margin at the bottom so that we can draw the key title
-    # afterwards.
+) -> memoryview:
+    """Render an image for a key."""
     if icon_filename is not None:
         icon = Image.open(ASSETS_PATH / icon_filename)
-    else:  # Open white image
+    else:
         icon = Image.new("RGB", (deck.KEY_PIXEL_WIDTH, deck.KEY_PIXEL_HEIGHT), "black")
-
     image = PILHelper.create_scaled_image(deck, icon, margins=[0, 0, 0, 0])
-
-    # Load a custom TrueType font and use it to overlay the key index, draw key
-    # label onto the image a few pixels from the bottom of the key.
     draw = ImageDraw.Draw(image)
     font = ImageFont.truetype(str(ASSETS_PATH / font_filename), 12)
     draw.text(
@@ -196,30 +203,29 @@ def render_key_image(
         font=font,
         anchor="ms",
         fill=text_color,
-        # Center
         align="center",
     )
-
     return PILHelper.to_native_format(deck, image)
 
 
 def update_key_image(
     deck: StreamDeck,
+    *,
     key: int,
     button: Button,
     state: dict[str, Any],
     key_pressed: bool = False,
-):
+) -> None:
+    """Update the image for a key."""
     if button.entity_id in state:
         state = state[button.entity_id]
         text = _render_jinja(button.text, data={"state": state})
         if button.text_color is not None:
             text_color = _render_jinja(button.text_color, data={"state": state})
+        elif state["state"] == "on":
+            text_color = "orangered"
         else:
-            if state["state"] == "on":
-                text_color = "orangered"
-            else:
-                text_color = "white"
+            text_color = "white"
     else:
         text = button.text
         text_color = button.text_color or "white"
@@ -245,34 +251,37 @@ def get_deck() -> StreamDeck:
         found = True
         break
     if not found:
-        raise RuntimeError("No Stream Deck found")
+        msg = "No Stream Deck found"
+        raise RuntimeError(msg)
     print(f"Found {deck.key_count()} keys, {deck=}")
     return deck
 
 
-def read_config(fname: str) -> Config:
-    with open(fname) as f:
+def read_config(fname: Path) -> Config:
+    """Read the configuration file."""
+    with fname.open() as f:
         return Config(buttons=yaml.safe_load(f))
 
 
-async def main(host: str, token: str, config: Config):
-    deck = get_deck()
-    buttons = config.buttons  # TODO: make it work with multiple decks
-    async with setup_ws(host, token) as websocket:
-        state = await get_states(websocket)
-        for key in range(deck.key_count()):
-            update_key_image(deck, key, buttons[key], state, key_pressed=False)
-        deck.set_key_callback_async(_on_press_callback(websocket, state, buttons))
-        deck.set_brightness(100)
-        await subscribe_state_changes(websocket)
-        await handle_state_changes(websocket, state, deck, buttons)
-
-
-def _on_press_callback(websocket, state, buttons):
-    async def key_change_callback(deck, key, key_pressed):
+def _on_press_callback(
+    websocket: websockets.WebSocketClientProtocol,
+    state: dict[str, Any],
+    buttons: list[Button],
+) -> Callable[[StreamDeck, int, bool], None]:
+    async def key_change_callback(
+        deck: StreamDeck,
+        key: int,
+        key_pressed: bool,  # noqa: FBT001
+    ) -> None:
         print(f"Key {key} {'pressed' if key_pressed else 'released'}")
         button = buttons[key]
-        update_key_image(deck, key, button, state, key_pressed)
+        update_key_image(
+            deck,
+            key=key,
+            button=button,
+            state=state,
+            key_pressed=key_pressed,
+        )
         if key_pressed:
             rich.print("Calling service", button.service)
             data = {"entity_id": button.entity_id} if button.entity_id else {}
@@ -281,13 +290,33 @@ def _on_press_callback(websocket, state, buttons):
     return key_change_callback
 
 
+async def main(host: str, token: str, config: Config) -> None:
+    """Main entry point for the Stream Deck integration."""
+    deck = get_deck()
+    buttons = config.buttons  # TODO: make it work with multiple decks
+    async with setup_ws(host, token) as websocket:
+        state = await get_states(websocket)
+        for key in range(deck.key_count()):
+            update_key_image(
+                deck,
+                key=key,
+                button=buttons[key],
+                state=state,
+                key_pressed=False,
+            )
+        deck.set_key_callback_async(_on_press_callback(websocket, state, buttons))
+        deck.set_brightness(100)
+        await subscribe_state_changes(websocket)
+        await handle_state_changes(websocket, state, deck, buttons)
+
+
 if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--host", default="localhost")
     parser.add_argument("--token", default=None)
-    parser.add_argument("--config", default="streamdeck-config.yaml")
+    parser.add_argument("--config", default="streamdeck-config.yaml", type=Path)
     args = parser.parse_args()
     config = read_config(args.config)
     asyncio.run(main(host=args.host, token=args.token, config=config))

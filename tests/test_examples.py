77f22fc074c264fafafd6315496632c32e8a1fef
Bas Nijholt <bas@nijho.lt>
2023-04-09 16:04:20 -0700

    Allow the ring image to be showed for a sensor/entity (#81)
    * Allow the ring image to be showed for a sensor/entity
    
    * wip
    
    * wip
    
    * Update README.md
    
    * rings
    
    * fix types
    
    * docs
    
    * Update README.md
    
    * more sensors
    
    * wip
    
    * wip
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1153,6 +1153,51 @@ start_timer = {
 }
 
 
+outside_temperature_display = {
+    "description": "🌡️ Display outside temperature with a ring indicator",
+    "extra": "This sets 0% to -10°C and 100% to 40°C.",
+    "yaml": textwrap.dedent(
+        """
+        - entity_id: sensor.temperature_sensor_outside_temperature
+          icon: >
+            {%- set temp = states('sensor.temperature_sensor_outside_temperature') -%}
+            {%- set min_temp = -10 -%}
+            {%- set max_temp = 40 -%}
+            {%- set pct = ((temp - min_temp) / (max_temp - min_temp)) * 100 -%}
+            ring:{{ pct | round }}
+          text: |
+            {%- set temp = states('sensor.temperature_sensor_outside_temperature') -%}
+            Outside
+            {{ temp | round(1) }}°C
+        """,
+    ),
+    "state": [
+        {
+            "sensor.temperature_sensor_outside_temperature": {
+                "state": "20",
+            },
+        },
+        {
+            "sensor.temperature_sensor_outside_temperature": {
+                "state": "10",
+            },
+        },
+    ],
+    "result": [
+        Button(
+            entity_id="sensor.temperature_sensor_outside_temperature",
+            icon="ring:60.0",
+            text="Outside\n20°C",
+        ),
+        Button(
+            entity_id="sensor.temperature_sensor_outside_temperature",
+            icon="ring:40.0",
+            text="Outside\n10°C",
+        ),
+    ],
+}
+
+
 BUTTONS = [
     activate_a_scene,
     toggle_a_cover,
@@ -1185,6 +1230,7 @@ BUTTONS = [
     change_cover_position,
     control_media_player_tv,
     start_timer,
+    outside_temperature_display,
 ]
 
 

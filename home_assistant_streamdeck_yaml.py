fbb2f5b05378d4f23da82d98cf52928a533ea035
Bas Nijholt <bas@nijho.lt>
2023-04-09 20:36:03 -0700

    Reload anonymous page when auto-reloading (#85)
    * Reload anonymous page when reloading
    
    * config update
    
    * Deal with removed pages

--- home_assistant_streamdeck_yaml.py
+++ home_assistant_streamdeck_yaml.py
@@ -565,8 +565,15 @@ class Config(BaseModel):
     def reload(self) -> None:
         """Reload the configuration file."""
         assert self._configuration_file is not None
+        # Updates all public attributes
         new = self.load(self._configuration_file).__dict__
         self.__dict__.update(new)
+        # Set the private attributes we want to preserve
+        if self._detached_page is not None:
+            self._detached_page = self.to_page(self._detached_page.name)
+        if self._current_page_index >= len(self.pages):
+            # In case pages were removed, reset to the first page
+            self._current_page_index = 0
 
     @classmethod
     def to_pandas_table(cls: type[Config]) -> pd.DataFrame:

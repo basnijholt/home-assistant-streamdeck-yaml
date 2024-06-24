basnijholt/home-assistant-streamdeck-yaml

commit 45ea80d86a807a1ce5e2466635ae069f755a0d32

Adding 2-way sync (SD-to-HAss) for state_entity_id (#184)
* Adding 2-way sync to state_entity_id

* call_service is async

* [pre-commit.ci] auto fixes from pre-commit.com hooks

for more information, see https://pre-commit.ci

* Factor out function

---------

Co-authored-by: Bas Nijholt <bas@nijho.lt>

home_assistant_streamdeck_yaml.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Home Assistant Stream Deck integration."""
+
 from __future__ import annotations
 
 import asyncio
@@ -1520,6 +1521,23 @@ def turn_off(config: Config, deck: StreamDeck) -> None:
     deck.set_brightness(0)
 
 
+async def _sync_input_boolean(
+    state_entity_id: str | None,
+    websocket: websockets.WebSocketClientProtocol,
+    state: Literal["on", "off"],
+) -> None:
+    """Sync the input boolean state with the Stream Deck."""
+    if (state_entity_id is not None) and (
+        state_entity_id.split(".")[0] == "input_boolean"
+    ):
+        await call_service(
+            websocket,
+            f"input_boolean.turn_{state}",
+            {},
+            {"entity_id": state_entity_id},
+        )
+
+
 async def _handle_key_press(
     websocket: websockets.WebSocketClientProtocol,
     complete_state: StateDict,
@@ -1529,7 +1547,7 @@ async def _handle_key_press(
 ) -> None:
     if not config._is_on:
         turn_on(config, deck, complete_state)
-        return
+        await _sync_input_boolean(config.state_entity_id, websocket, "on")
 
     def update_all() -> None:
         deck.reset()
@@ -1548,6 +1566,7 @@ async def _handle_key_press(
         return  # to skip the _detached_page reset below
     elif button.special_type == "turn-off":
         turn_off(config, deck)
+        await _sync_input_boolean(config.state_entity_id, websocket, "off")
     elif button.special_type == "light-control":
         assert isinstance(button.special_type_data, dict)
         page = _light_page(

commit 6c5726645812a104165018e7742bbb77d8535f6a

Return early when turning on the StreamDeck (#190)
Closes #186

Reverts problem introduced in https://github.com/basnijholt/home-assistant-streamdeck-yaml/pull/184. 

Thanks to https://github.com/basnijholt/home-assistant-streamdeck-yaml/issues/186#issuecomment-2180458308.

home_assistant_streamdeck_yaml.py
@@ -2051,6 +2051,7 @@ async def handle_dial_event(
     if not config._is_on:
         turn_on(config, deck, complete_state)
         await _sync_input_boolean(config.state_entity_id, websocket, "on")
+        return
 
     if dial[0].dial_event_type == event_type.name:
         selected_dial = dial[0]
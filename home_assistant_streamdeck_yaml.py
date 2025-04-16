d09dcb8af1deb4ec89e564029aa32fc35912aa03
ngolf <74095787+ngolf@users.noreply.github.com>
2025-04-16 14:45:43 +0100

    Added comment to climate function

--- home_assistant_streamdeck_yaml.py
+++ home_assistant_streamdeck_yaml.py
@@ -84,6 +84,7 @@ StateDict: TypeAlias = dict[str, dict[str, Any]]
 # Globals or context-level shared state
 is_network_connected: bool = False
 
+# Gets the climate icon, text, and text color for climate modes
 def get_climate_icon_text_and_color(mode: str) -> tuple[str, str, str]:
     cool_color = "cyan"
     cool_text = "cool"
